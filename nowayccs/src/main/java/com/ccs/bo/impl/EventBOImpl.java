89489c25d3931317f2d5e58414a090c6ce9c7524
fotb <fotb@fotb-pc>
2018-12-02 01:07:52 +0800

    7

--- nowayccs/src/main/java/com/ccs/bo/impl/EventBOImpl.java
+++ nowayccs/src/main/java/com/ccs/bo/impl/EventBOImpl.java
@@ -162,7 +162,7 @@ public class EventBOImpl implements IEventBO {
 		        		
 		        if(isSuccess){
 		        	logger.info("success to upload with serialNumber " + vo.getSerialNumber());
-		        	 logger.error("upload info fail with result: " + result);
+		        	 logger.error("upload info success with result: " + result);
 		        	vo.setUpStatus(EventVO.UP_STATUS_1);
 		        } else {
 		            logger.info("fail to upload with serialNumber: " + vo.getSerialNumber());
