19c2ede477f77cdeda33dea7286f113a9bb72f0f
fotb <fotb@192.168.2.177>
2019-04-23 00:41:02 +0800

    update

---
+++ nowayccs/src/main/java/com/ccs/services/vo/IssueNew.java
@@ -0,0 +1,23 @@
+package com.ccs.services.vo;
+
+import java.io.Serializable;
+
+public class IssueNew implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3902443850974722512L;
+	
+	private String serialNumber;
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	
+
+}
